48f0e739a3c8d9e22cd35e4b170ae98a26390f98
qistinazhar <145536087+qistinazhar@users.noreply.github.com>
2023-09-26 15:20:39 +0800

    info_form ver1.0     initial     upload image form

--- info_form.php
+++ info_form.php
@@ -1,10 +1,90 @@
 <!--
     info_form ver1.0
     initial
+    upload image form
 -->
 
 <?php 
   $user_name = get_post_meta( get_the_ID(), 'name', true);
+
+if ($_SERVER["REQUEST_METHOD"] == "POST" && isset($_FILES["image"])) {
+    $targetDirectory = "uploads/"; // Create a directory called "uploads" in the same directory as this script
+    $targetFile = $targetDirectory . basename($_FILES["image"]["name"]);
+    $uploadOk = true;
+
+    // Check if the file is an image
+    $imageFileType = strtolower(pathinfo($targetFile, PATHINFO_EXTENSION));
+    if (!in_array($imageFileType, ['jpg', 'jpeg', 'png', 'gif'])) {
+        echo "Sorry, only JPG, JPEG, PNG, and GIF files are allowed.";
+        $uploadOk = false;
+    }
+
+    // Check if the file already exists
+    if (file_exists($targetFile)) {
+        echo "Sorry, the file already exists.";
+        $uploadOk = false;
+    }
+
+    // Check file size (optional)
+    if ($_FILES["image"]["size"] > 500000) {
+        echo "Sorry, your file is too large.";
+        $uploadOk = false;
+    }
+
+    if ($uploadOk) {
+        // Upload the file
+        if (move_uploaded_file($_FILES["image"]["tmp_name"], $targetFile)) {
+            echo "The file " . basename($_FILES["image"]["name"]) . " has been uploaded.";
+
+            // Check the file type and resize accordingly
+            if (in_array($imageFileType, ['jpg', 'jpeg'])) {
+                resizeJPGImage($targetFile, 1024, 1024, 60);
+            } elseif ($imageFileType === 'png') {
+                resizePNGImage($targetFile, 512, 512);
+            }
+        } else {
+            echo "Sorry, there was an error uploading your file.";
+        }
+    }
+} else {
+    echo "Invalid request.";
+}
+
+function resizeJPGImage($filename, $max_width, $max_height, $quality) {
+    list($orig_width, $orig_height) = getimagesize($filename);
+    $ratio = min($max_width / $orig_width, $max_height / $orig_height);
+
+    $new_width = $orig_width * $ratio;
+    $new_height = $orig_height * $ratio;
+
+    $image_p = imagecreatetruecolor($new_width, $new_height);
+    $image = imagecreatefromjpeg($filename);
+
+    imagecopyresampled($image_p, $image, 0, 0, 0, 0, $new_width, $new_height, $orig_width, $orig_height);
+
+    imagejpeg($image_p, $filename, $quality);
+    imagedestroy($image_p);
+}
+
+function resizePNGImage($filename, $max_width, $max_height) {
+    list($orig_width, $orig_height) = getimagesize($filename);
+    $ratio = min($max_width / $orig_width, $max_height / $orig_height);
+
+    $new_width = $orig_width * $ratio;
+    $new_height = $orig_height * $ratio;
+
+    $image_p = imagecreatetruecolor($new_width, $new_height);
+    $image = imagecreatefrompng($filename);
+
+    imagealphablending($image_p, false);
+    imagesavealpha($image_p, true);
+    imagecopyresampled($image_p, $image, 0, 0, 0, 0, $new_width, $new_height, $orig_width, $orig_height);
+
+    imagepng($image_p, $filename);
+    imagedestroy($image_p);
+}
+
+
 ?>
 <!DOCTYPE html>
 <html >
@@ -17,127 +97,31 @@
 	</head>
 	<body>
 <div class="form">
-    <div >
+    <div style="width:100%;">
 		<image src= "https://arlink.arleta.my/wp-content/uploads/2023/09/background-1-150x150.jpg" class="background">	</image>
-	<svg xmlns="http://www.w3.org/2000/svg" width="100%">
-	<!--<rect fill="#ffffff" width="540" height="100">
-	</rect><defs><linearGradient id="a" 
-	gradientUnits="userSpaceOnUse" x1="0" x2="0"
-	y1="0" y2="100%" gradientTransform="rotate(222,648,379)">
-	<stop offset="0" stop-color="#ffffff"></stop><stop 
-	offset="1" stop-color="#FC726E"></stop>
-	</linearGradient><pattern
-	patternUnits="userSpaceOnUse" id="b" width="300" 
-	height="250" x="0" y="0" viewBox="0 0 1080 900"><g 
-	fill-opacity="0.5"><polygon fill="#444" points="90
-	150 0 300 180 300"></polygon><polygon points="90
-	150 180 0 0 0"></polygon><polygon fill="#AAA" 
-	points="270 150 360 0 180 0"></polygon><polygon 
-	fill="#DDD" points="450 150 360 300 540 300">
-	</polygon><polygon fill="#999" points="450 150 540 
-	0 360 0"></polygon><polygon points="630 150 540 300
-	720 300"></polygon><polygon fill="#DDD" points="630
-	150 720 0 540 0"></polygon><polygon fill="#444" 
-	points="810 150 720 300 900 300"></polygon><polygon
-	fill="#FFF" points="810 150 900 0 720 0">
-	</polygon><polygon fill="#DDD" points="990 150 900
-	300 1080 300"></polygon><polygon fill="#444" points="990 150
-	1080 0 900 0"></polygon><polygon fill="#DDD" 
-	points="90 450 0 600 180 600"></polygon><polygon
-	points="90 450 180 300 0 300"></polygon><polygon
-	fill="#666" points="270 450 180 600 360 600">
-	</polygon><polygon fill="#AAA" points="270 450 360 
-	300 180 300"></polygon><polygon fill="#DDD"
-	points="450 450 360 600 540 600">
-	</polygon><polygon fill="#999" points="450 450 540
-	300 360 300"></polygon><polygon fill="#999"
-	points="630 450 540 600 720 600"></polygon><polygon
-	fill="#FFF" points="630 450 720 300 540 300">
-	</polygon><polygon points="810 450 720 
-	600 900 600"></polygon><polygon fill="#DDD" 
-	points="810 450 900 300 720 300">
-	</polygon><polygon fill="#AAA" points="990 450 900
-	600 1080 600"></polygon><polygon fill="#444"
-	points="990 450 1080 300 900 300"></polygon><polygon 
-	fill="#222" points="90 750 0 900 180 900">
-	</polygon><polygon points="270 750 180 
-	900 360 900"></polygon><polygon fill="#DDD"
-	points="270 750 360 600 180 600"></polygon><polygon
-	points="450 750 540 600 360 600"></polygon><polygon 
-	points="630 750 540 900 720 900"></polygon><polygon 
-	fill="#444" points="630 750 720 600 540 600">
-	</polygon><polygon fill="#AAA" points="810 750 720
-	900 900 900"></polygon><polygon fill="#666" 
-	points="810 750 900 600 720 600"></polygon><polygon
-	fill="#999" points="990 750 900 900 1080 900">
-	</polygon><polygon fill="#999"
-	points="180 0 90 150 270 150"></polygon><polygon
-	fill="#444" points="360 0 270 150 450 150">
-	</polygon><polygon fill="#FFF" points="540 0 450
-	150 630 150"></polygon><polygon points="900 0 810 150 990 150">
-	</polygon><polygon fill="#222"
-	points="0 300 -90 450 90 450"></polygon><polygon fill="#FFF"
-	points="0 300 90 150 -90 150"></polygon><polygon fill="#FFF"
-	points="180 300 90 450 270 450"></polygon><polygon 
-	fill="#666" points="180 300 270 150 90 150">
-	</polygon><polygon fill="#222" points="360 300 270 450 450 450">
-	</polygon><polygon fill="#FFF"
-	points="360 300 450 150 270 150"></polygon><polygon
-	fill="#444" points="540 300 450 450 630 450">
-	</polygon><polygon fill="#222"
-	points="540 300 630 150 450 150"></polygon><polygon
-	fill="#AAA" points="720 300 630 450 810 450">
-	</polygon><polygon fill="#666"
-	points="720 300 810 150 630 150"></polygon><polygon
-	fill="#FFF" points="900 300 810 450 990 450">
-	</polygon><polygon fill="#999" 
-	points="900 300 990 150 810 150">
-	</polygon><polygon points="0 600 -90 750 90 750">
-	</polygon><polygon fill="#666" 
-	points="0 600 90 450 -90 450">
-	</polygon><polygon fill="#AAA" 
-	points="180 600 90 750 270 750"></polygon><polygon
-	fill="#444" points="180 600 270 450 90 450"></polygon><polygon
-	fill="#444" points="360 600 270 750 450 750">
-	</polygon><polygon fill="#999"
-	points="360 600 450 450 270 450">
-	</polygon><polygon fill="#666"
-	points="540 600 630 450 450 450"></polygon><polygon
-	fill="#222" points="720 600 630 750 810 750">
-	</polygon><polygon fill="#FFF" 
-	points="900 600 810 750 990 750"></polygon><polygon
-	fill="#222" points="900 600 990 450 810 450"></polygon><polygon
-	fill="#DDD" points="0 900 90 750 -90 750">
-	</polygon><polygon fill="#444" points="180 900 270
-	750 90 750"></polygon><polygon fill="#FFF
-	points="360 900 450 750 270 750"></polygon><polygon
-	fill="#AAA" points="540 900 630 750 450 750">
-	</polygon><polygon fill="#FFF" points="720 900 810
-	750 630 750"></polygon><polygon fill="#222"
-	points="900 900 990 750 810 750">
-	</polygon><polygon fill="#222
-	points="1080 300 990 450 1170 450">
-	</polygon><polygon fill="#FFF"
-	points="1080 300 1170 150 990 150"></polygon><polygon
-	points="1080 600 990 750 1170 750"></polygon><polygon
-	fill="#666" points="1080 600 1170 450 990 450">
-	</polygon><polygon fill="#DDD"
-	points="1080 900 1170 750 990 750">
-	</polygon></g>
-	</pattern></defs>
-	<rect x="0" y="0" fill="url(#a)" width="100%" height="100%">
-	</rect><rect x="0" y="0" fill="url(#b)" width="100%" height="100%"></rect>--></svg></div>
+	</div>
+
     <div class="form__avatar">
 	<image src= "https://arlink.arleta.my/wp-content/uploads/2023/09/user.png" 	class="username">	
 	</div>
+
     <div class="form__title">Fill the form</div>
+
     <div class="form__subtitle">Information</div>
+
     <div class="form__wrapper">
         <input type="text" placeholder="Name" id="name_textbox"><br>
         <input type="text" placeholder="Occupation" id="occupation_textbox"><br>
         <textarea required="required" id="shortDesc_textarea"></textarea> 
 		<span>short decription..</span><br>
         
+        <h2>Upload an Image</h2>
+    <form action="upload.php" method="post" enctype="multipart/form-data">
+        <label for="image">Select an image:</label>
+        <input type="file" name="image" id="image" accept="image/*">
+        <input type="submit" value="Upload Image">
+    </form>
+
 		<div>
 			<button type="button" onclick="setData()" class="form__btn">Save</button>
 		</div>
@@ -146,12 +130,14 @@
 			<p>Your data successfully saved!</p>
 			<button type="button" onclick="closePopup()">OK</button>
 		</div>
-		<div>
+
+		
+        <div style="display:flex;">
 			<a class="form__btn form__btn-solid" href="https://chat.openai.com/">chatGpt</a>
-<a class="form__btn form__btn-solid" href="https://wa.link/l0wpiw">Whatsapp</a>
+            <a class="form__btn form__btn-solid" href="https://wa.link/l0wpiw">Whatsapp</a>
 		</div>
     </div>
-</div><br><br><br><br><br><br><br><br><br><br><br><br>
+</div>
 
 <script>
 		let popup = document.getElementById("popup");
@@ -233,10 +219,6 @@ function setData() {
   top: calc(50% -20px);
 }
 
-/*.form__avatar svg {
-  width: 100px;
-  height: 100px;
-}*/
 
 .form__title {
   margin-top: 60px;
@@ -324,7 +306,7 @@ textarea[required="required"]{
 	text-align:center;
     padding: 8px;
 	text-decoration: none;
-	margin-left: 250px;
+	margin:10px;
 
 }
 </style>
